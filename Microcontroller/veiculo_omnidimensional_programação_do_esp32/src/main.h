3147baecddbb57fafe45a548f44e834fea397a60
Marcio Bulla <marciobulla@ufpr.br>
2024-07-31 17:35:05 -0300

    Ajustes codigo e criando compile_commands.json

---
+++ Microcontroller/veiculo_omnidimensional_programação_do_esp32/src/main.h
@@ -0,0 +1,8 @@
+
+void girar_motor_frente(int variavel1, int variavel2, int variavel3,
+                        int variavel4, int pino_velocidade1,
+                        int pino_velocidade2, int velocidade);
+
+void girar_motor_tras(int variavel11, int variavel22, int variavel33,
+                      int variavel44, int pino_velocidade1,
+                      int pino_velocidade2, int velocidade1);
